33acc862100bdaeefa7527025fed333851ab5566
Bernd Schwarzenbacher <bernd.schwb@yahoo.com>
2016-07-08 16:09:59 +0200

    Finish first implementation of graph laplacian
    Passes all tests.
    Correct SquareTest to include all edges of a square.

--- header/graph_laplacian.hpp
+++ header/graph_laplacian.hpp
@@ -1,6 +1,8 @@
 #ifndef GRAPH_LAPLACIAN_HPP_
 #define GRAPH_LAPLACIAN_HPP_
 
+#include <tuple>
+
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/graph_traits.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
@@ -10,10 +12,28 @@ using Matrix = boost::numeric::ublas::matrix<int>;
 
 Matrix graph_laplacian(const Graph& g)
 {
-  using size_type = graph_traits<Graph>::vertices_size_type;
+  using namespace boost;
+  const auto nv = num_vertices(g);
+  Matrix mat(nv, nv, 0);
 
-  const size_type nv = num_vertices(g);
-  return Matrix(nv, nv, 0);
+  auto index = get(vertex_index, g);
+
+  using vertex_iter = graph_traits<Graph>::vertex_iterator;
+  vertex_iter vi, vi_end;
+
+  using adj_iter = graph_traits<Graph>::adjacency_iterator;
+  adj_iter ai, ai_end;
+
+  for (std::tie(vi, vi_end) = vertices(g); vi != vi_end; ++vi) {
+    auto v = *vi;
+    auto v_index = index[v];
+    mat(v_index, v_index) = degree(v, g);
+    for (std::tie(ai, ai_end) = adjacent_vertices(v, g); ai != ai_end; ++ai) {
+      mat(v_index, index[*ai]) = -1;
+    }
+  }
+
+  return mat;
 }
 
 #endif  // GRAPH_LAPLACIAN_HPP_
--- tests/test_graph_laplacian.cpp
+++ tests/test_graph_laplacian.cpp
@@ -5,24 +5,22 @@
 #include <vector>
 
 #include <boost/graph/adjacency_list.hpp>
-using namespace boost;
 
 #include <boost/numeric/ublas/assignment.hpp>
 #include <boost/numeric/ublas/io.hpp>
 #include <boost/numeric/ublas/matrix.hpp>
-using namespace boost::numeric::ublas;
 
 #include <graph_laplacian.hpp>
 
 // An matrix equality check copied from: http://lists.boost.org/Archives/boost/2005/11/96235.php
 template<class E>
-bool equal(matrix<E> lhs, matrix<E> rhs)
+bool equal(boost::numeric::ublas::matrix<E> lhs, boost::numeric::ublas::matrix<E> rhs)
 {
   if (&lhs == &rhs) { return true; }
   if (lhs.size1() != rhs.size1()) { return false; }
   if (lhs.size2() != rhs.size2()) { return false; }
-  typename matrix<E>::iterator1 l(lhs.begin1());
-  typename matrix<E>::iterator1 r(rhs.begin1());
+  typename boost::numeric::ublas::matrix<E>::iterator1 l(lhs.begin1());
+  typename boost::numeric::ublas::matrix<E>::iterator1 r(rhs.begin1());
   while (l != lhs.end1()) {
     if (*l != *r) { return false; }
     ++l;
@@ -38,7 +36,7 @@ BOOST_AUTO_TEST_CASE(EmptyGraphTest)
 {
   Graph g;
 
-  BOOST_CHECK(equal(graph_laplacian(g), matrix<int>(0, 0)));
+  BOOST_CHECK(equal(graph_laplacian(g), boost::numeric::ublas::matrix<int>(0, 0)));
 }
 
 // A graph with n vertices and no edges produces an (n x n)-zero matrix
@@ -46,82 +44,82 @@ BOOST_AUTO_TEST_CASE(NoEdgesTest)
 {
   Graph g(5);
 
-  BOOST_CHECK(equal(graph_laplacian(g), matrix<int>(5, 5, 0)));
+  BOOST_CHECK(equal(graph_laplacian(g), boost::numeric::ublas::matrix<int>(5, 5, 0)));
 }
 
-//// A graph with two vertices and an edge connecting them produces the following Laplacian matrix:
-////  1 -1
-//// -1  1
-//BOOST_AUTO_TEST_CASE(TwoVerticesOneEdgeGraphTest)
-//{
-  //using Edge = std::pair<int, int>;
-  //std::vector<Edge> edges { Edge(0, 1) };
-  //Graph g(edges.begin(), edges.end(), 2);
+// A graph with two vertices and an edge connecting them produces the following Laplacian matrix:
+//  1 -1
+// -1  1
+BOOST_AUTO_TEST_CASE(TwoVerticesOneEdgeGraphTest)
+{
+  using Edge = std::pair<int, int>;
+  std::vector<Edge> edges { Edge(0, 1) };
+  Graph g(edges.begin(), edges.end(), 2);
 
-  //matrix<int> expected_mat(2, 2);
-  //expected_mat <<=  1, -1,
-                   //-1,  1;
+  boost::numeric::ublas::matrix<int> expected_mat(2, 2);
+  expected_mat <<=  1, -1,
+                   -1,  1;
 
-  //BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
-//}
+  BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
+}
 
-//// A free tree on 3 vertices (vertex 1 is the center vertex) produces following Laplacian matrix:
-////  1 -1  0
-//// -1  2 -1
-////  0 -1  1
-//BOOST_AUTO_TEST_CASE(ThreeVerticesFreeTreeTest)
-//{
-  //using Edge = std::pair<int, int>;
-  //std::vector<Edge> edges { Edge(0, 1), Edge(1, 2) };
-  //Graph g(edges.begin(), edges.end(), 3);
+// A free tree on 3 vertices (vertex 1 is the center vertex) produces following Laplacian matrix:
+//  1 -1  0
+// -1  2 -1
+//  0 -1  1
+BOOST_AUTO_TEST_CASE(ThreeVerticesFreeTreeTest)
+{
+  using Edge = std::pair<int, int>;
+  std::vector<Edge> edges { Edge(0, 1), Edge(1, 2) };
+  Graph g(edges.begin(), edges.end(), 3);
 
-  //matrix<int> expected_mat(3, 3);
-  //expected_mat <<=  1, -1,  0,
-                   //-1,  2, -1,
-                    //0, -1,  1;
+  boost::numeric::ublas::matrix<int> expected_mat(3, 3);
+  expected_mat <<=  1, -1,  0,
+                   -1,  2, -1,
+                    0, -1,  1;
 
-  //BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
-//}
+  BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
+}
 
-//// A free tree on 4 vertices (vertices 1, 2 are the middle vertices) produces following
-//// Laplacian matrix:
-////  1 -1  0  0
-//// -1  2 -1  0
-////  0 -1  2 -1
-////  0  0 -1  1
-//BOOST_AUTO_TEST_CASE(FourVerticesFreeTreeTest)
-//{
-  //using Edge = std::pair<int, int>;
-  //std::vector<Edge> edges { Edge(0, 1), Edge(1, 2), Edge(2, 3) };
-  //Graph g(edges.begin(), edges.end(), 4);
+// A free tree on 4 vertices (vertices 1, 2 are the middle vertices) produces following
+// Laplacian matrix:
+//  1 -1  0  0
+// -1  2 -1  0
+//  0 -1  2 -1
+//  0  0 -1  1
+BOOST_AUTO_TEST_CASE(FourVerticesFreeTreeTest)
+{
+  using Edge = std::pair<int, int>;
+  std::vector<Edge> edges { Edge(0, 1), Edge(1, 2), Edge(2, 3) };
+  Graph g(edges.begin(), edges.end(), 4);
 
-  //matrix<int> expected_mat(4, 4);
-  //expected_mat <<=  1, -1,  0,  0,
-                   //-1,  2, -1,  0,
-                    //0, -1,  2, -1,
-                    //0,  0, -1,  1;
+  boost::numeric::ublas::matrix<int> expected_mat(4, 4);
+  expected_mat <<=  1, -1,  0,  0,
+                   -1,  2, -1,  0,
+                    0, -1,  2, -1,
+                    0,  0, -1,  1;
 
-  //BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
-//}
+  BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
+}
 
-//// A graph forming a square with 4 vertices produces follwoing Laplacian matrix:
-////  2 -1  0 -1
-//// -1  2 -1  0
-////  0 -1  2 -1
-//// -1  0 -1  2
-//BOOST_AUTO_TEST_CASE(SquareTest)
-//{
-  //using Edge = std::pair<int, int>;
-  //std::vector<Edge> edges { Edge(0, 1), Edge(1, 2), Edge(2, 3) };
-  //Graph g(edges.begin(), edges.end(), 4);
+// A graph forming a square with 4 vertices produces follwoing Laplacian matrix:
+//  2 -1  0 -1
+// -1  2 -1  0
+//  0 -1  2 -1
+// -1  0 -1  2
+BOOST_AUTO_TEST_CASE(SquareTest)
+{
+  using Edge = std::pair<int, int>;
+  std::vector<Edge> edges { Edge(0, 1), Edge(1, 2), Edge(2, 3), Edge(3, 0) };
+  Graph g(edges.begin(), edges.end(), 4);
 
-  //matrix<int> expected_mat(4, 4);
-  //expected_mat <<=  2, -1,  0, -1,
-                   //-1,  2, -1,  0,
-                    //0, -1,  2, -1,
-                   //-1,  0, -1,  2;
+  boost::numeric::ublas::matrix<int> expected_mat(4, 4);
+  expected_mat <<=  2, -1,  0, -1,
+                   -1,  2, -1,  0,
+                    0, -1,  2, -1,
+                   -1,  0, -1,  2;
 
-  //BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
-//}
+  BOOST_CHECK(equal(graph_laplacian(g), expected_mat));
+}
 
 BOOST_AUTO_TEST_SUITE_END()  // TestGraphLaplacian
